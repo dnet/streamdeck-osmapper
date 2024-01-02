4c27c24a965bedcdc41c0b01eafbb817d103bfb5
András Veres-Szentkirályi <vsza@vsza.hu>
2024-01-02 17:39:47 +0100

    put images into array

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@ use unbounded_gpsd::GpsdConnection;
 use anyhow::Result;
 use streamdeck::StreamDeck;
 
+const BUTTONS: [&str; 6] = ["fire_hydrant.png", "bicycle-parking.png",
+    "corrosion.png", "bump.png", "szaglocso.png", "stop.png"];
+
 fn main() -> Result<()> {
     //let gpsd = GpsdConnection::new("127.0.0.1:2947")?;
     println!("Hello, world!");
     let mut sd = StreamDeck::connect(0x0fd9, 0x006d, None)?;
     sd.reset()?;
-    sd.set_button_file(0, "fire_hydrant.png", &streamdeck::images::ImageOptions::default())?;
-    sd.set_button_file(1, "bicycle-parking.png", &streamdeck::images::ImageOptions::default())?;
-    sd.set_button_file(2, "corrosion.png", &streamdeck::images::ImageOptions::default())?;
-    sd.set_button_file(3, "bump.png", &streamdeck::images::ImageOptions::default())?;
-    sd.set_button_file(4, "szaglocso.png", &streamdeck::images::ImageOptions::default())?;
-    sd.set_button_file(5, "stop.png", &streamdeck::images::ImageOptions::default())?;
+    for (key, image) in BUTTONS.iter().enumerate() {
+        sd.set_button_file(key as u8, image, &streamdeck::images::ImageOptions::default())?;
+    }
     // TODO time, number of sats, speed into text tiles
     // TODO watch buttons
     // TODO poll gpsd
